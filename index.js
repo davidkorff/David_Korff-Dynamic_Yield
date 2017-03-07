c3827d7beea4f5e4030384d40055423ee51c21c6
davidkorff <davidkorff@gmail.com>
2017-03-06 22:39:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+window.onload=prepareFrame()
+function prepareFrame() {
+        name = prompt("Please enter your name", "")
+        if (name!=="" && name!=="null"){
+          var iframe = document.createElement("iframe")
+          iframe.setAttribute("src", "frame.html")
+          iframe.setAttribute("name", `${name}`)
+          iframe.setAttribute("id", `${name}`)
+          iframe.style.width = "500px"
+          iframe.style.height = "500px"
+          document.body.appendChild(iframe)
+          sendJoinMessage(`${name}`)
+      }
+    }
+
+window.addEventListener('message', receiveMessage)
+
+function receiveMessage(){
+  var array = document.getElementsByTagName("iframe")
+  for (let i = 0; i<array.length; i++){
+    var receiver = array[i].contentWindow
+    receiver.postMessage(event.data, '*');
+    // sendMessage(event.data, receiver)
+  }
+}
+
+function sendJoinMessage(name){
+  var array = document.getElementsByTagName("iframe")
+  for (let i = 0; i<array.length; i++){
+    var receiver = array[i].contentWindow
+    receiver.postMessage(`[system] - ${name} joined the conversation<br>`, '*')
+    // sendMessage(event.data, receiver)
+  }
+}
